5f0cdf990744cd3db000048d5c0fe664d7e6541d
Christian Orthmann <christian.orthmann@hotmail.com>
2016-06-21 22:38:12 +0200

    fix railtie configuration

--- lib/eeny-meeny/railtie.rb
+++ lib/eeny-meeny/railtie.rb
@@ -4,12 +4,12 @@ require 'eeny-meeny/middleware'
 module EenyMeeny
   class Railtie < Rails::Railtie
     config.eeny_meeny = ActiveSupport::OrderedOptions.new
+    # default config values. these can be changed in the rails environment configuration.
+    config.eeny_meeny.experiments = []
+    config.eeny_meeny.secure = true
+    config.eeny_meeny.secret = '9fc8b966eca7d03d55df40c01c10b8e02bf1f9d12d27b8968d53eb53e8c239902d00bf6afae5e726ce1111159eeb2f8f0e77233405db1d82dd71397f651a0a4f'
 
     initializer 'eeny_meeny.initialize' do |app|
-      config.eeny_meeny.experiments ||= [] # experiements need to be loaded from config... yaml is a good idea?
-      config.eeny_meeny.secure ||= true
-      config.eeny_meeny.secret ||= '9fc8b966eca7d03d55df40c01c10b8e02bf1f9d12d27b8968d53eb53e8c239902d00bf6afae5e726ce1111159eeb2f8f0e77233405db1d82dd71397f651a0a4f'
-
       ActionController::Base.send :include, EenyMeeny::ExperimentHelper
       ActionView::Base.send :include, EenyMeeny::ExperimentHelper
 
